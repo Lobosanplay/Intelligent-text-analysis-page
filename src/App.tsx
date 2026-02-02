58ca18852d6f2cbbfc719b9f1fb483e2cb1daf32
Lobosanplay <parra.santiago2008@gmail.com>
2026-02-02 11:16:37 -0500

    Init dev enviroment

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="h-screen bg-gray-500">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold text-white">
+          Intelligent text analysis
+        </h1>
+        <span className="justify-center mt-3">
+          Intelligent text analysis It is a program that will help you create
+          summaries efficiently and understand their fundamental topics.
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
