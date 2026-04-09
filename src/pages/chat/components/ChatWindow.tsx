9b3c83bddc4b47fd975c2f39e3cea0e0033f0b7c
Lobosanplay <parra.santiago2008@gmail.com>
2026-04-09 17:12:03 -0500

    style: Adding collapse sidebar and change view of messages.

--- src/pages/chat/components/ChatWindow.tsx
+++ src/pages/chat/components/ChatWindow.tsx
@@ -8,9 +8,11 @@ import {
 } from "../../../shared/services/chat/chatService";
 import { useNavigate } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useAuth } from "../../../shared/hooks/useAuth";
 
 export default function ChatWindow() {
   const { chatId } = useParams();
+  const { username } = useAuth();
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
@@ -22,14 +24,14 @@ export default function ChatWindow() {
     enabled: !!chatId,
   });
 
+  const hasMessages = messages.length > 0;
+
   const { mutateAsync: createChatMutation } = useMutation({
     mutationFn: (data: CreateMessage) => chatService.createChatService(data),
 
     onSuccess: (newMessage) => {
       const newChatId = newMessage.message.conversation_id;
-
       queryClient.invalidateQueries({ queryKey: ["chats"] });
-
       navigate(`/dashboard/chat/${newChatId}`);
     },
   });
@@ -61,18 +63,31 @@ export default function ChatWindow() {
   const bottomRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messages]);
+    if (hasMessages) {
+      bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages, hasMessages]);
 
   return (
-    <div className="flex flex-col justify-center text-center h-full">
-      <div className="flex-1 overflow-y-auto px-6 py-6 space-y-4">
-        {messages.map((m, i) => (
-          <MessageBubble key={i} message={m} />
-        ))}
-
-        <div ref={bottomRef} />
-      </div>
+    <div
+      className={`h-full flex flex-col ${
+        hasMessages ? "" : "items-center justify-center"
+      }`}
+    >
+      {hasMessages ? (
+        <div className="flex-1 overflow-y-auto px-6 py-6 ">
+          <div className="max-w-3xl mx-auto space-y-4 w-full mb-20">
+            {messages.map((m, i) => (
+              <MessageBubble key={i} message={m} />
+            ))}
+            <div ref={bottomRef} />
+          </div>
+        </div>
+      ) : (
+        <div className="absolute top-55">
+          <h3 className="text-3xl text-white">How can I help, {username}?</h3>
+        </div>
+      )}
 
       <ChatInput onSend={sendMessage} />
     </div>
--- src/pages/dashboard/components/layout/DashboardLayout.tsx
+++ src/pages/dashboard/components/layout/DashboardLayout.tsx
@@ -1,13 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../sidebar/Sidebar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { PanelLeftClose, PanelLeftOpen } from "lucide-react";
 
+const queryClient = new QueryClient();
 export default function DashboardLayout() {
-  const queryClient = new QueryClient();
+  const [collapsed, setCollapsed] = useState(true);
+
   return (
     <div className="flex h-screen bg-black text-white">
       <QueryClientProvider client={queryClient}>
-        <Sidebar />
+        <button
+          onClick={() => setCollapsed(!collapsed)}
+          className={`absolute z-50 hover:bg-neutral-800 p-2 rounded ${collapsed ? "top-1 left-5" : "top-5 left-50"}`}
+        >
+          {collapsed ? <PanelLeftOpen /> : <PanelLeftClose />}
+        </button>
+
+        <Sidebar collapsed={collapsed} />
 
         <main className="flex-1 overflow-y-auto">
           <Outlet />
