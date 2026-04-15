b24cc0cd5c329973d9d3a06d772dd7f759b4eb04
Lobosanplay <parra.santiago2008@gmail.com>
2026-04-15 15:31:27 -0500

    feat: modal to search chat by title

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import { AuthContextProvider } from "./shared/providers/auth.provider";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthContextProvider>
-      <RouterProvider router={router} />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
